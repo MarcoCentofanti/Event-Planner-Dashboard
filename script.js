279084ac6616a95eac8efda84b6b7902424ac5b7
marcocentofanti <marcocentofanti@hotmail.it>
2026-01-18 19:57:34 +0100

    add dark mode style

--- script.js
+++ script.js
@@ -1,4 +1,60 @@
+// COLOR MODE
+const darkModeButton = document.getElementById("darkModeButton")
 
+
+const bodyElement = document.querySelector("body")
+const navbarElement = document.querySelector("nav")
+const textAll = document.querySelectorAll("label, p, h1, h2, h3, h4, h5")
+const cardAll = document.getElementById("eventList")
+const selectAll = document.querySelectorAll("select")
+const formButton = document.querySelectorAll("button.formButton")
+
+darkModeButton.addEventListener("click", (e => {
+  if (darkModeButton.textContent === "Dark Mode") {
+    darkModeButton.textContent = "Light Mode"
+  } else {
+    darkModeButton.textContent = "Dark Mode"
+  }
+  const cardBody = document.querySelectorAll("div.card-body")
+  bodyElement.classList.toggle("bg-dark")
+  navbarElement.classList.toggle("bg-light")  
+  navbarElement.classList.toggle("bg-dark")
+  navbarElement.classList.toggle("navbar-light")
+  navbarElement.classList.toggle("navbar-dark")
+  darkModeButton.classList.toggle("btn-dark")
+  darkModeButton.classList.toggle("btn-light")
+  titleEvent.classList.toggle("bg-dark")
+  titleEvent.classList.toggle("text-white")
+
+  cardAll.classList.toggle("text-white")
+  cardAll.classList.toggle("bg-dark")
+  
+  textAll.forEach(element => {
+    element.classList.toggle("text-white")
+  }); 
+  
+  cardBody.forEach(element => {
+    element.classList.toggle("text-white")
+    element.classList.toggle("bg-dark")
+  }); 
+
+  selectAll.forEach(element => {
+    element.classList.toggle("text-white")
+    element.classList.toggle("bg-dark")
+  }); 
+
+  formButton.forEach(element => {
+    element.classList.toggle("btn-secondary")
+    element.classList.toggle("btn-outline-light")
+  }); 
+
+
+}))
+
+
+
+
+// EVENT FUNCTIONALITY
 const agendaArray = []
 
 const submitButton = document.getElementById("submitButton")
